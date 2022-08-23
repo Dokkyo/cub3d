64cb3813d80ef67f83e58c149f35b0d58e7dbb9c
bifrah <benj.ifrah@gmail.com>
2022-08-23 16:25:01 +0200

    add logique de parsing

--- cub3d.h
+++ cub3d.h
@@ -24,3 +24,19 @@ int		ft_parse(t_cub3d *game);
 int		ft_strcmp(char *s1, char *s2);
 
 #endif
+
+/*
+*
+*	Lors du parsing de la map :
+*	- Creer un int (compteur)
+*	- Pour le remplir, il suffirait de checket grace a un tableau de chaine de caractere (char**) qui contiendrait chaque identifieur.
+*		Lors du scan du fichier, des qu'on rencontre un identifieur, ajouter +1 au compteur.
+*		Save cette ligne pour checker son format et la validite par la suite.
+*
+*	Quand le compteur arrive a 6, on n'est cense rencontrer que des isspace (ou des /n) ou des 1/0 (debut de map).
+*
+*	Si l'on rencontre tout autre caractere (excepte isspace) avant un 1/0 : map invalide.
+*	Lors de la rencontre d'un 1 ou d'un 0, la map commence.
+*		Normalement, La premiere et deniere ligne sont seulement des 1 (pour une map ferme).
+*
+*/
