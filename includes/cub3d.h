c8e9243a6da57fe5c6e8c3ab2bad41fd82fa899b
bifrah <benj.ifrah@gmail.com>
2022-09-07 14:34:10 +0200

    norme

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -1,17 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bifrah <bifrah@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/07 14:33:59 by bifrah            #+#    #+#             */
+/*   Updated: 2022/09/07 14:33:59 by bifrah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CUB3D_H
 # define CUB3D_H
 
-#include "../mlx_linux/mlx.h"
-#include "X11/X.h"
-#include "../libft/libft.h"
+# include "../mlx_linux/mlx.h"
+# include "X11/X.h"
+# include "../libft/libft.h"
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <math.h>
-#include <fcntl.h>
-#include <stdbool.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <errno.h>
+# include <math.h>
+# include <fcntl.h>
+# include <stdbool.h>
 
 //check path
 //check rgb split sur virgule -> if size return split == 3 check value 0to255.
@@ -58,35 +70,35 @@ typedef enum e_errno
 /****************************************************/
 /*		Keycodes									*/
 /****************************************************/
-#define W 119
-#define A 97
-#define S 115
-#define D 100
-#define ESC 65307
-#define LEFT 65361
-#define RIGHT 65363
+# define W 119
+# define A 97
+# define S 115
+# define D 100
+# define ESC 65307
+# define LEFT 65361
+# define RIGHT 65363
 
 /****************************************************/
 /*		Config for the player						*/
 /****************************************************/
-#define CUB_SIZE 64
-#define WIN_SIZE 1024
-#define FOV 66.0
-#define ROTATION_UNIT 10
-#define MOVEMENT_UNIT 20
-#define BASE_DEC "0123456789"
-#define BASE_DEC_SPACE "0123456789\t\n\r\v\f "
+# define CUB_SIZE 64
+# define WIN_SIZE 1024
+# define FOV 66.0
+# define ROTATION_UNIT 10
+# define MOVEMENT_UNIT 20
+# define BASE_DEC "0123456789"
+# define BASE_DEC_SPACE "0123456789\t\n\r\v\f "
 
 /****************************************************/
 /*		All mlx image's useful data in one place	*/
 /****************************************************/
 typedef struct s_image
 {
-	void	*img;
-	int		*addr;
-	int		bpp;
-	int		ll;
-	int		endian;
+	void			*img;
+	int				*addr;
+	int				bpp;
+	int				ll;
+	int				endian;
 }		t_image;
 
 /****************************************************/
@@ -197,7 +209,6 @@ typedef struct s_cub3d
 
 }		t_cub3d;
 
-
 /*	Ajoutez les protos de cette maniere :
 **
 **	*************************************
@@ -216,69 +227,69 @@ typedef struct s_cub3d
 /****************************************/
 
 //	check_map.c
-void	check_map(t_cub3d *game);
-void	get_player_game(t_player *player, int x, int y, char c);
+void			check_map(t_cub3d *game);
+void			get_player_game(t_player *player, int x, int y, char c);
 
 //	get_identifiers.c
-void	get_identifiers(t_cub3d *game, char *line);
+void			get_identifiers(t_cub3d *game, char *line);
 
 //	get_map.c
-void	get_map(t_cub3d *game, int *fd, char **line, int map_start);
+void			get_map(t_cub3d *game, int *fd, char **line, int map_start);
 
 //	parse.c
-int		open_map(t_cub3d *game, char *filename);
-void	parse(t_cub3d *game, char *filename);
+int				open_map(t_cub3d *game, char *filename);
+void			parse(t_cub3d *game, char *filename);
 
 /****************************************/
 /*				utils					*/
 /****************************************/
 
 // 	array.c
-int		length_2d_array(char **a);
-void	free_2d_array(char **a);
-void	skip_digits(char **s);
-void	skip_spaces(char **s);
+int				length_2d_array(char **a);
+void			free_2d_array(char **a);
+void			skip_digits(char **s);
+void			skip_spaces(char **s);
 
 //	check_space.c
-void	check_space(t_cub3d *game, char **map, int row, int col);
+void			check_space(t_cub3d *game, char **map, int row, int col);
 
 // 	cub3d_exit.c
-void	exit_cub3d(t_cub3d *game, int my_errno);
+void			exit_cub3d(t_cub3d *game, int my_errno);
 
 // 	cub3d_init.c
-void	init_cub3d(t_cub3d *game, char *filename);
+void			init_cub3d(t_cub3d *game, char *filename);
 
 // 	ft_isin.c
-bool	ft_isin(const char *s, const char *accepted);
+bool			ft_isin(const char *s, const char *accepted);
 
 // 	ft_strcmp.c
-int		ft_strcmp(char *s1, char *s2);
+int				ft_strcmp(char *s1, char *s2);
 
 // 	ft_strnspn.c
-int		ft_strcspn(const char *s, const char *rejected);
-int		ft_strcspn2(const char *s, const char *rejected);
+int				ft_strcspn(const char *s, const char *rejected);
+int				ft_strcspn2(const char *s, const char *rejected);
 
 //	mlx.c
-void	get_mlx_instance(t_cub3d *game);
-void	get_mlx_window(t_cub3d *game);
-void	get_mlx_image(t_cub3d *game);
+void			get_mlx_instance(t_cub3d *game);
+void			get_mlx_window(t_cub3d *game);
+void			get_mlx_image(t_cub3d *game);
 
 //	pixel.c
-void	pixel_to_image(t_image *image, int x, int y, int color);
-int		get_image_pixel_color(t_image *image, int col, int row);
+void			pixel_to_image(t_image *image, int x, int y, int color);
+int				get_image_pixel_color(t_image *image, int col, int row);
 
 /****************************************/
 /*				raycasting				*/
 /****************************************/
 
 //	graphics_effects.c
-void	textured_column_to_image(t_cub3d *game);
+void			textured_column_to_image(t_cub3d *game);
 
 //	hooks.c
-void	define_hooks(t_cub3d *game);
+void			define_hooks(t_cub3d *game);
 
 //	move.c
-void	move(t_cub3d *game, double dir);
+void			move(t_cub3d *game, double dir);
 
 //	raycasting.c
 t_int_vector	get_step(double ray);
@@ -286,27 +297,10 @@ void			dda(t_cub3d *game);
 void			raycasting(t_cub3d *game);
 
 //	rotate.c
-double	rotate(double ray, double angle);
-void	rotate_player(t_player *player, double angle);
+double			rotate(double ray, double angle);
+void			rotate_player(t_player *player, double angle);
 
-// int		ft_parse(t_cub3d *game);
-// int		ft_strcmp(char *s1, char *s2);
+// int				ft_parse(t_cub3d *game);
+// int				ft_strcmp(char *s1, char *s2);
 
 #endif
-
-/*
-*
-*	Lors du parsing de la map :
-*	- Creer un int (compteur)
-*	- Pour le remplir, il suffirait de checket grace a un tableau de chaine de caractere (char**) qui contiendrait chaque identifieur.
-*		Lors du scan du fichier, des qu'on rencontre un identifieur, ajouter +1 au compteur.
-*		Save cette ligne pour checker son format et la validite par la suite.
-*
-*	Quand le compteur arrive a 6, on n'est cense rencontrer que des isspace (ou des /n) ou des 1/0 (debut de map).
-*
-*	Si l'on rencontre tout autre caractere (excepte isspace) avant un 1/0 : map invalide.
-*	Lors de la rencontre d'un 1 ou d'un 0, la map commence.
-*		Normalement, La premiere et deniere ligne sont seulement des 1 (pour une map ferme).
-*		Le premier et dernier octet de chaque ligne contient soit un isspace soit un 1.
-*
-*/
